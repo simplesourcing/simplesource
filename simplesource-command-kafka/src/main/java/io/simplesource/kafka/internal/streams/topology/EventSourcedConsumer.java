4c432d88b686e11b173f94d0c7242b7ea6fbc21c
szoio <stephenzoio@yahoo..com>
2018-11-07 20:06:21 +1100

    started idempotence

--- simplesource-command-kafka/src/main/java/io/simplesource/kafka/internal/streams/topology/EventSourcedConsumer.java
+++ simplesource-command-kafka/src/main/java/io/simplesource/kafka/internal/streams/topology/EventSourcedConsumer.java
@@ -1,14 +1,20 @@
 package io.simplesource.kafka.internal.streams.topology;
 
 import io.simplesource.kafka.model.CommandRequest;
+import io.simplesource.kafka.model.CommandResponse;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.KStream;
 
 import static io.simplesource.kafka.api.AggregateResources.TopicEntity.command_request;
+import static io.simplesource.kafka.api.AggregateResources.TopicEntity.command_response;
 
 final class EventSourcedConsumer {
 
     static <K, C> KStream<K, CommandRequest<C>> commandRequestStream(TopologyContext<K, C, ?, ?> ctx, final StreamsBuilder builder) {
-        return builder.stream(ctx.topicName(command_request), ctx.commandEventsConsumed());
+        return builder.stream(ctx.topicName(command_request), ctx.commandRequestConsumed());
+    }
+
+    static <K, C> KStream<K, CommandResponse> commandResponseStream(TopologyContext<K, C, ?, ?> ctx, final StreamsBuilder builder) {
+        return builder.stream(ctx.topicName(command_response), ctx.commandResponseConsumed());
     }
 }
--- simplesource-command-kafka/src/main/java/io/simplesource/kafka/internal/streams/topology/EventSourcedStreams.java
+++ simplesource-command-kafka/src/main/java/io/simplesource/kafka/internal/streams/topology/EventSourcedStreams.java
@@ -2,16 +2,55 @@ package io.simplesource.kafka.internal.streams.topology;
 
 import io.simplesource.api.CommandError;
 import io.simplesource.data.Result;
+import io.simplesource.kafka.internal.util.Tuple;
 import io.simplesource.kafka.model.*;
+import org.apache.kafka.streams.kstream.Joined;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Serialized;
+import org.apache.kafka.streams.processor.Processor;
+import org.apache.kafka.streams.processor.ProcessorContext;
+import org.apache.kafka.streams.state.KeyValueStore;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.UUID;
 import java.util.function.BiFunction;
 
 import static io.simplesource.kafka.api.AggregateResources.StateStoreEntity.aggregate_update;
 
 final class EventSourcedStreams {
+    private static <K> long getResponseSequence(CommandResponse response) {
+        return response.sequenceResult().getOrElse(response.readSequence()).getSeq();
+    }
+
+    static <K, C, E, A> KTable<UUID, CommandResponse> commandResponseTable(TopologyContext<K, C, E, A> ctx, final KStream<K, CommandResponse> commandResponseStream) {
+
+
+
+        return null        ;
+    }
+
+    static <K, C, E, A> void getProcessedCommands(TopologyContext<K, C, E, A> ctx,
+                                                  final KStream<K, CommandRequest<C>> commandRequestStream,
+                                                  final KStream<K, CommandResponse> commandResponseStream) {
+
+        KTable<UUID, CommandResponse> commandResponseById = commandResponseStream
+                .selectKey((key, response) -> response.commandId())
+                .groupByKey(Serialized.with(ctx.serdes().commandResponseKey(), ctx.serdes().commandResponse()))
+                .reduce((r1, r2) -> getResponseSequence(r1) > getResponseSequence(r2) ? r1 : r2);
+
+        KTable<K, CommandResponse> commandResponseByAggKey = commandResponseStream
+                .groupByKey(Serialized.with(ctx.serdes().aggregateKey(), ctx.serdes().commandResponse()))
+                .reduce((r1, r2) -> getResponseSequence(r1) > getResponseSequence(r2) ? r1 : r2);
+
+        commandRequestStream
+                .selectKey((k, v) -> v.commandId())
+                .leftJoin(commandResponseById, Tuple::new, Joined.with(ctx.serdes().commandResponseKey(), ctx.serdes().commandRequest(), ctx.serdes().commandResponse()))
+                .selectKey((k, v) -> v.v1());
+    }
+
+
 
     static <K, C, E, A> KStream<K, CommandEvents<E, A>> eventResultStream(TopologyContext<K, C, E, A> ctx, final KStream<K, CommandRequest<C>> commandRequestStream) {
         return commandRequestStream
