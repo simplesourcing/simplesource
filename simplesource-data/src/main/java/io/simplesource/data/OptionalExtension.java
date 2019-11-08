8396956053f29251fb4656b47e9710e4230e62b1
jules Ivanic <jules.ivanic@gmail.com>
2019-11-08 14:44:47 +1100

    Make fold lazy

--- simplesource-data/src/main/java/io/simplesource/data/OptionalExtension.java
+++ simplesource-data/src/main/java/io/simplesource/data/OptionalExtension.java
@@ -2,13 +2,14 @@ package io.simplesource.data;
 
 import java.util.Optional;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class OptionalExtension {
     public static <A, B> Optional<Pair<A, B>> zip(Optional<A> maybeA, Optional<B> maybeB) {
         return maybeA.flatMap((A a) -> maybeB.map((B b) -> Pair.of(a, b)));
     }
 
-    public static <A, R> R fold(Optional<A> maybeA, R onNone, Function<A, R> onSome) {
-        return maybeA.map(onSome).orElse(onNone);
+    public static <A, R> R fold(Optional<A> maybeA, Supplier<R> onNone, Function<A, R> onSome) {
+        return maybeA.map(onSome).orElseGet(onNone);
     }
 }
