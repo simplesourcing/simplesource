21b1fe98c61469a6667bdebbc5472f9ece252e29
szoio <stephenzoio@yahoo.com>
2019-11-21 13:57:54 +1100

    added requireNotStarted

--- simplesource-command-kafka/src/main/java/io/simplesource/kafka/dsl/EventSourcedApp.java
+++ simplesource-command-kafka/src/main/java/io/simplesource/kafka/dsl/EventSourcedApp.java
@@ -1,19 +1,11 @@
 package io.simplesource.kafka.dsl;
 
-import io.simplesource.api.CommandAPI;
-import io.simplesource.kafka.internal.client.KafkaCommandAPI;
 import io.simplesource.kafka.internal.streams.EventSourcedStreamsApp;
-import io.simplesource.kafka.internal.util.NamedThreadFactory;
 import io.simplesource.kafka.spec.AggregateSetSpec;
 import io.simplesource.kafka.spec.AggregateSpec;
-import io.simplesource.kafka.spec.CommandSpec;
-import io.simplesource.kafka.util.SpecUtils;
-import org.apache.kafka.streams.errors.StreamsException;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
@@ -26,17 +18,20 @@ public final class EventSourcedApp {
 
     public EventSourcedApp withKafkaConfig(
             final Function<KafkaConfig.Builder, KafkaConfig> builder) {
+        requireNotStarted();
         kafkaConfig = builder.apply(new KafkaConfig.Builder());
         return this;
     }
 
     public EventSourcedApp withKafkaConfig(final KafkaConfig kafkaConfig) {
+        requireNotStarted();
         this.kafkaConfig = kafkaConfig;
         return this;
     }
 
     public <K, C, E, A> EventSourcedApp withAggregate(
             final Function<AggregateBuilder<K, C, E, A>, AggregateSpec<K, C, E, A>> buildSteps) {
+        requireNotStarted();
         AggregateBuilder<K, C, E, A> builder = AggregateBuilder.newBuilder();
         final AggregateSpec<K, C, E, A> spec = buildSteps.apply(builder);
         aggregateConfigMap.put(spec.aggregateName(), spec);
@@ -44,12 +39,14 @@ public final class EventSourcedApp {
     }
 
     public <K, C, E, A> EventSourcedApp withAggregate(final AggregateSpec<K, C, E, A> spec) {
+        requireNotStarted();
         aggregateConfigMap.put(spec.aggregateName(), spec);
         return this;
     }
 
     public <K, E> EventSourcedApp withEvent(
             final Consumer<EventAggregateBuilder<K, E>> buildSteps) {
+        requireNotStarted();
         EventAggregateBuilder<K, E> builder = EventAggregateBuilder.newBuilder();
         buildSteps.accept(builder);
         final AggregateSpec<K, E, E, ?> spec = builder.build();
@@ -58,6 +55,7 @@ public final class EventSourcedApp {
     }
 
     public <K, E> EventSourcedApp withEvent(final AggregateSpec<K, E, E, ?> spec) {
+        requireNotStarted();
         aggregateConfigMap.put(spec.aggregateName(), spec);
         return this;
     }
@@ -73,5 +71,9 @@ public final class EventSourcedApp {
         EventSourcedStreamsApp app = new EventSourcedStreamsApp(aggregateSetSpec);
         app.start();
         isStarted = true;
+    }
+
+    private void requireNotStarted() {
+        if (isStarted ) throw new RuntimeException("App already started, and cannot be modified.");
     }
 }
